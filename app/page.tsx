f5d47305e64e88ea11860c71bb3c7d287a9ef655
Jakub Jeziorski <106917056+Kuba-Jeziorski@users.noreply.github.com>
2026-09-07 22:33:35 +0100

    feat: update HomePage component to redirect to projects and add metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from "next";
+import { redirect } from "next/navigation";
+
+export const metadata: Metadata = {
+  title: "Kuba-Jeziorski Portfolio",
+  description: "Welcome page",
+};
+
 export default function HomePage() {
-  return <h1>Homepage</h1>;
+  redirect("/projects");
 }
